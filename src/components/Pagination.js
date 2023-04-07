8cfed0f51d1cf8704919cddffb134b8a9695ab92
vinicius <vmm_7@hotmail.com>
2023-04-06 21:15:16 -0300

    Fix: pagination issue in Netlify

--- src/components/Pagination.js
+++ src/components/Pagination.js
@@ -14,9 +14,11 @@ export const Pagination = ({ usuariosPorPagina, usuariosTotais, paginate }) => {
   return (
     <nav className="d-flex justify-content-center mt-4">
       <ul className="pagination">
-        {numeroDePaginas.map((numero) => (
+        {numeroDePaginas.map(numero => (
           <li key={numero} className="page-item">
-            <a onClick={()=> paginate(numero)} href="!#" className="page-link">{numero}</a>
+            <a onClick={() => paginate(numero)} href="/#" className="page-link">
+              {numero}
+            </a>
           </li>
         ))}
       </ul>
